4915e7259b9c4969c31876538474ea71acc28f18
Ellis7886 <ellis7886@gmail.com>
2026-05-06 13:28:44 +0800

    Initial Ecolearn project

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcoLearnApp());
+}
+
+class EcoLearnApp extends StatelessWidget {
+  const EcoLearnApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Image.asset('assets/images/logo.jpeg', height:300,),
+
+            Container(
+              width: 400,
+              padding: const EdgeInsets.all(20),
+              
+              decoration: BoxDecoration(
+                color: const Color(0xFF2B2B2B),
+                borderRadius: BorderRadius.circular(20)
+              ),
+
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.menu_book,
+                    color: Colors.green,
+                    size:40,
+                  ),
+
+                  SizedBox(width:20),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+
+                    children: [
+                      Text(
+                          'Lessons',
+                          style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+
+                      Text(
+                        'Access learning materials',
+                        style: TextStyle(color: Colors.white70)
+                      )
+                    ],
+                  )
+                ],
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
