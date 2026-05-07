fa92a181fb5728aeb8beb4b086bfabfec7d6b720
Ellis7886 <92918862+Ellis7886@users.noreply.github.com>
2026-05-07 16:06:32 +0800

    App icon and Splash screen have been solved

--- lib/main.dart
+++ lib/main.dart
@@ -25,51 +25,59 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
 
-          children: [
-            Image.asset('assets/images/ecolearn_logo.png', height:300,),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
 
-            Container(
-              width: 400,
-              padding: const EdgeInsets.all(20),
-              
-              decoration: BoxDecoration(
-                color: const Color(0xFF2B2B2B),
-                borderRadius: BorderRadius.circular(20)
-              ),
+              children: [
+                Center(
+                  child: Image.asset('assets/images/ecolearn_logo.png', height:300)
+                ),
 
-              child: const Row(
-                children: [
-                  Icon(
-                    Icons.menu_book,
-                    color: Colors.green,
-                    size:50,
+                Container(
+                  width: 400,
+                  padding: const EdgeInsets.all(20),
+
+                  decoration: BoxDecoration(
+                    color: Color(0xFF2B2B2B),
+                    borderRadius: BorderRadius.circular(20)
                   ),
 
-                  SizedBox(width:20),
-
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-
+                  child: const Row(
                     children: [
-                      Text(
-                          'Lessons',
-                          style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                      Icon(
+                        Icons.menu_book,
+                        color: Color(0xFF9BD028),
+                        size:50,
                       ),
 
-                      Text(
-                        'Access learning materials',
-                        style: TextStyle(color: Colors.white70)
+                      SizedBox(width:20),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Text(
+                              'Lessons',
+                              style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+
+                          Text(
+                            'Access learning materials',
+                            style: TextStyle(color: Colors.white70)
+                          )
+                        ],
                       )
                     ],
                   )
-                ],
-              )
-            ),
-          ],
+                ),
+              ],
+            )
+          )
         ),
       ),
     );
