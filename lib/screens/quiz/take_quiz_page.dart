6e0c5493fb51eee3ccf84647746886441ecd8805
Ellis7886 <ellis7886@gmail.com>
2026-09-21 15:01:51 +0800

    Final version of prototype with the automated script

--- lib/screens/quiz/take_quiz_page.dart
+++ lib/screens/quiz/take_quiz_page.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:provider/provider.dart';
-
-import '../../provider/app_settings.dart';
-
-import '../../controllers/quiz_controller.dart';
 
 import '../../services/quiz_service.dart';
 
